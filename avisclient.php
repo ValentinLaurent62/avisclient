71aea25b02bc9446a7e495bf3c458531003e5f63
Valentin Laurent <valentin_laurent@ens.univ-artois.fr>
2019-04-02 20:44:35 +0200

    commit initial

---
+++ avisclient.php
@@ -0,0 +1,39 @@
+<?php
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class AvisClient extends Module
+{
+    
+    public function __construct()
+    {
+        // infos module
+        $this->name = "avisclient";
+        $this->tab = 'front_office_features';
+        $this->version = '1.0';
+        $this->author = 'Valentin Laurent';
+
+        // config
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = [
+            'min' => '1.6',
+            'max' => _PS_VERSION_
+        ];
+        $this->bootstrap = true;
+
+        // constructeur parent
+        parent::__construct();
+
+        // affichage
+        $this->displayName = $this->l('Avis client');
+        $this->description = $this->l('Permet à l\'administrateur de renseigner les avis des clients, afin qu\'un de ceux-ci soit affiché aléatoirement sur la page d\'accueil.');
+
+        $this->confirmUninstall = $this->l('Souhaitez-vous réellemment désinstaller ce module ?');
+
+        if (!Configuration::get('MYMODULE_NAME')) {
+            $this->warning = $this->l('Aucun nom renseigné.');
+        }
+    }
+}
