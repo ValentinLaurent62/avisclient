f9b2a4a719a96ba8b9a5f2617bdbf1776348e917
ValentinLaurent62 <valentin.lau62@gmail.com>
2019-04-05 14:37:00 +0200

    ajout du model avis

---
+++ Avis.php
@@ -0,0 +1,18 @@
+<?php
+class Avis extends ObjectModel
+{
+    public $id_avis;
+    public $titre;
+    public $contenu;
+    public $date_ajout;
+
+    public static $definition = array(
+        'table' => 'avis',
+        'primary' => 'id_avis',
+        'fields' => array(
+            'titre' => array('type' => self::TYPE_STRING, 'required' => TRUE),
+            'contenu' => array('type' => self::TYPE_STRING, 'required' => TRUE),
+            'date_ajout' => array('type' => self::TYPE_DATE, 'validate' => 'isDateFormat')
+        )
+    );
+}
